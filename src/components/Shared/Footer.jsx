c66e3abb26be0277797439710088575a0c8652dd
BrunoWolfsteiner <brunowolfsteiner0@gmail.com>
2023-01-08 00:49:48 -0300

    first commit

---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer-bg'>
+      <div className="footer-copy">© Academlo 2022 by Bruno Wolfsteiner</div>
+    </footer>
+  );
+};
+
+export default Footer;
